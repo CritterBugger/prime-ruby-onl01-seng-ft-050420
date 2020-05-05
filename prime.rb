d0fee485e38e4b1adc83c4b9e67c6b5a16267d27
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 16:13:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(num)
+  return false if num <= 1 
+    
+  div = (num/2).round
+  
+  while div > 1
+    if num % div == 0
+      return false
+    end
+    
+    div -= 1
+  end
+  
+  return true
+end
